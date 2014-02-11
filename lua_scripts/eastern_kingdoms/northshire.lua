50942e3b63ba7aa2ccb46e4ff3675f8ffe88927f
Salja <salja8840@gmail.com>
2014-02-11 22:52:06 +0100

    Support for NPC "Kobold Vermin" and "Defias Thug"
    Signed-off-by: Salja <salja8840@gmail.com>

---
+++ lua_scripts/eastern_kingdoms/northshire.lua
@@ -0,0 +1,58 @@
+--
+-- Kobold Vermin - Random Say on Aggro
+-- NPC Entry 6
+-- Script Complete 100%
+--
+
+NPC_KOBOLD_VERMIN = 6
+
+local SAY_KOBOLD_VERMIN_1 = "You no take candle!"
+local SAY_KOBOLD_VERMIN_2 = "Yiieeeee! Me run!"
+
+local function KoboldVerminOnEnterCombat(event, creature, target)
+    local chance = math.random(1, 2)
+
+    if (chance == 1) then
+        creature:SendUnitSay(SAY_KOBOLD_VERMIN_1, 0)
+    elseif (chance == 2) then
+        creature:SendUnitSay(SAY_KOBOLD_VERMIN_2, 0)
+    end
+end
+
+local function KoboldVerminOnLeaveCombat(event, creature)
+    creature:RemoveEvents()
+end
+
+RegisterCreatureEvent(NPC_KOBOLD_VERMIN, 1, KoboldVerminOnEnterCombat)
+RegisterCreatureEvent(NPC_KOBOLD_VERMIN, 2, KoboldVerminOnLeaveCombat)
+
+--
+-- Defias Thug - Random Say on Aggro
+-- NPC Entry 38
+-- Script Complete 100%
+--
+
+NPC_DEFIAS_THUG = 38
+
+local SAY_DEFIAS_THUG_1 = "The Brotherhood will not tolerate your actions."
+local SAY_DEFIAS_THUG_2 = "Ah, a chance to use this freshly sharpened blade."
+local SAY_DEFIAS_THUG_3 = "Feel the power of the Brotherhood!"
+
+local function DefiasThugOnEnterCombat(event, creature, target)
+    local chance = math.random(1, 3)
+
+    if (chance == 1) then
+        creature:SendUnitSay(SAY_DEFIAS_THUG_1, 0)
+    elseif (chance == 2) then
+        creature:SendUnitSay(SAY_DEFIAS_THUG_2, 0)
+    elseif (chance == 3) then
+        creature:SendUnitSay(SAY_DEFIAS_THUG_3, 0)
+    end
+end
+
+local function DefiasThugOnLeaveCombat(event, creature)
+    creature:RemoveEvents()
+end
+
+RegisterCreatureEvent(NPC_DEFIAS_THUG, 1, DefiasThugOnEnterCombat)
+RegisterCreatureEvent(NPC_DEFIAS_THUG, 2, DefiasThugOnLeaveCombat)
