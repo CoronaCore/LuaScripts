9cc6a092fe8159e981ca456a0b424ac72ba0c1f5
Salja <salja8840@gmail.com>
2014-02-17 15:12:58 +0100

    Update PvP Token System
    Signed-off-by: Salja <salja8840@gmail.com>

--- lua_scripts/custom/pvp_token.lua
+++ lua_scripts/custom/pvp_token.lua
@@ -1,8 +1,15 @@
 local ItemEntry = 20558 -- Warsong Gulch Mark of Honor
 local ItemCount = 1
+local ItemName = GetItemLink(ItemEntry)
 
 local function PvPTokenItem(event, killer, killed)
     killer:AddItem(ItemEntry, ItemCount)
+
+    if (ItemCount == 1) then
+         killer:SendBroadcastMessage("|CFF20C000 You create: "..ItemName.."|CFF20C000.|r")
+    else
+         killer:SendBroadcastMessage("|CFF20C000 You create: "..ItemName.."|CFF20C000 x"..ItemCount..".|r")
+    end
 end
 
 RegisterPlayerEvent(6, PvPTokenItem)
