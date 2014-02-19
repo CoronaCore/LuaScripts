8e3369e83e9402294a3b4aae3557e781fae81dfb
Salja <salja8840@gmail.com>
2014-02-20 00:08:46 +0100

    some notes
    Signed-off-by: Salja <salja8840@gmail.com>

--- lua_scripts/custom/premium.lua
+++ lua_scripts/custom/premium.lua
@@ -1,4 +1,6 @@
-local function PremiumOnLogin(event, player)
+-- NOTE: need to insert character_premium.sql in your characters database
+
+local function PremiumOnLogin(event, player)  -- Send a welcome massage to player and tell him is premium or not
     local result = CharDBQuery("SELECT AccountId FROM premium WHERE active=1 and AccountId = "..player:GetAccountId())
     if (result) then
         player:SendBroadcastMessage("|CFFE55BB0[Premium]|r|CFFFE8A0E Welcome "..player:GetName().." you are Premium! |r")
@@ -9,7 +11,7 @@ end
 
 local function PremiumOnChat(event, player, msg, _, lang)
     local result = CharDBQuery("SELECT AccountId FROM premium WHERE active=1 and AccountId = "..player:GetAccountId())
-    if (msg == "#premium") then
+    if (msg == "#premium") then  -- Use #premium for sending the gossip menu
         if (result) then
             OnPremiumHello(event, player)
         else
@@ -23,21 +25,23 @@ function OnPremiumHello(event, player)
     player:GossipMenuAddItem(0, "Show Bank", 0, 3)
     player:GossipMenuAddItem(0, "Show AuctionsHouse", 0, 4)
     player:GossipMenuAddItem(0, "Nevermind..", 0, 1)
+    -- Room for more premium things
     player:GossipSendMenu(1, player, 100)
 end
 
 function OnPremiumSelect(event, player, _, sender, intid, code)
-    if (intid == 1) then
+    if (intid == 1) then                     -- Close the Gossip
         player:GossipComplete()
-    elseif (intid == 2) then
+    elseif (intid == 2) then                 -- Go back to main menu
         OnPremiumHello(event, player)
-    elseif (intid == 3) then
+    elseif (intid == 3) then                 -- Send Bank Window
         player:SendShowBank(player)
-    elseif (intid == 4) then
+    elseif (intid == 4) then                 -- Send Auctions Window
         player:SendAuctionMenu(player)
     end
+    -- Room for more premium things
 end
 
-RegisterPlayerEvent(3, PremiumOnLogin)
-RegisterPlayerEvent(18, PremiumOnChat)
-RegisterPlayerGossipEvent(100, 2, OnPremiumSelect)
+RegisterPlayerEvent(3, PremiumOnLogin)              -- Register Event On Login
+RegisterPlayerEvent(18, PremiumOnChat)              -- Register Evenet on Chat Command use
+RegisterPlayerGossipEvent(100, 2, OnPremiumSelect)  -- Register Event for Gossip Select
