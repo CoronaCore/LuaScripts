aef52bb508b3d21a62328e5bbfb1a72e40da96e5
Salja <salja8840@gmail.com>
2014-02-20 17:49:55 +0100

    Update "item_exchange.lua" rename to "npc_exchange.lua" added new methods for "player:GossipSetText" thanks rochet
    Signed-off-by: Salja <salja8840@gmail.com>

--- lua_scripts/base/sc_default.lua
+++ lua_scripts/base/sc_default.lua
@@ -1,4 +1,4 @@
---
+﻿--
 -- Cooldown Methods by Foereaper
 --
 -- player:SetLuaCooldown(seconds) -- Sets the cooldown timer to X seconds
@@ -34,4 +34,34 @@ function Player:GetLuaCooldown()
     else
         return cooldowns[guid][source] - os.clock();
     end
+end
+
+--
+-- GossipSetText Methods by Rochet2
+--
+-- player:GossipMenuAddItem(0, "Text", 0, 0)
+-- player:GossipMenuAddItem(0, "Text", 0, 0)
+-- player:GossipSetText("Text")
+-- player:GossipSendMenu(0x7FFFFFFF, creature)
+--
+
+local SMSG_NPC_TEXT_UPDATE = 384
+local MAX_GOSSIP_TEXT_OPTIONS = 8
+
+function Player:GossipSetText(text)
+    data = CreatePacket(SMSG_NPC_TEXT_UPDATE, 100);
+    data:WriteULong(0x7FFFFFFF)
+    for i = 1, MAX_GOSSIP_TEXT_OPTIONS do
+        data:WriteFloat(0)     -- Probability
+        data:WriteString(text) -- Text
+        data:WriteString(text) -- Text
+        data:WriteULong(0)     -- language
+        data:WriteULong(0)     -- emote
+        data:WriteULong(0)     -- emote
+        data:WriteULong(0)     -- emote
+        data:WriteULong(0)     -- emote
+        data:WriteULong(0)     -- emote
+        data:WriteULong(0)     -- emote
+    end
+    self:SendPacketToPlayer(data)
 end
