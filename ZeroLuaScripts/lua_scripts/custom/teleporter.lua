330c6bc8f017f05e80cfb6859235f146f1db33f1
Salja <salja8840@gmail.com>
2014-03-07 15:08:13 +0100

    CodingStyle for Teleporters
    Signed-off-by: Salja <salja8840@gmail.com>

--- ZeroLuaScripts/lua_scripts/custom/teleporter.lua
+++ ZeroLuaScripts/lua_scripts/custom/teleporter.lua
@@ -22,59 +22,61 @@
 local UnitEntry =
  
 local T = {
-        [1] = { "Horde Cities", 1,
-                {"Orgrimmar", 1, 1503, -4415.5, 22, 0},
-                {"Undercity", 0, 1831, 238.5, 61.6, 0},
-                {"Thunderbluff", 1, -1278, 122, 132, 0},
-        },
-        [2] = { "Alliance Cities", 0,
-                {"Stormwind", 0, -8905, 560, 94, 0.62},
-                {"Ironforge", 0, -4795, -1117, 499, 0},
-                {"Darnassus", 1, 9952, 2280.5, 1342, 1.6},
-        },
-        [3] = { "PvP Locations", 2,
-                {"Gurubashi Arena", 0, -13229, 226, 33, 1},
-                {"Dire Maul Arena", 1, -3669, 1094, 160, 3},
-        },
+    [1] = { "Horde Cities", 1,
+        {"Orgrimmar", 1, 1503, -4415.5, 22, 0},
+        {"Undercity", 0, 1831, 238.5, 61.6, 0},
+        {"Thunderbluff", 1, -1278, 122, 132, 0},
+    },
+
+    [2] = { "Alliance Cities", 0,
+        {"Stormwind", 0, -8905, 560, 94, 0.62},
+        {"Ironforge", 0, -4795, -1117, 499, 0},
+        {"Darnassus", 1, 9952, 2280.5, 1342, 1.6},
+    },
+
+    [3] = { "PvP Locations", 2,
+        {"Gurubashi Arena", 0, -13229, 226, 33, 1},
+        {"Dire Maul Arena", 1, -3669, 1094, 160, 3},
+    },
 };
  
 --[[ CODE STUFFS! DO NOT EDIT BELOW ]]--
 --[[ UNLESS YOU KNOW WHAT YOU'RE DOING! ]]--
  
 function Teleporter_Gossip(event, player, unit)
-        if (#T <= 10) then
-                for i, v in ipairs(T) do
-                        if(v[2] == 2 or v[2] == player:GetTeam()) then
-                                player:GossipMenuAddItem(0, v[1], 0, i)
-                        end
-                end
-                player:GossipSendMenu(1, unit)
-        else
-                print("This teleporter only supports 10 different menus.")
+    if (#T <= 10) then
+        for i, v in ipairs(T) do
+            if(v[2] == 2 or v[2] == player:GetTeam()) then
+                player:GossipMenuAddItem(0, v[1], 0, i)
+            end
         end
+        player:GossipSendMenu(1, unit)
+    else
+       print("This teleporter only supports 10 different menus.")
+    end
 end    
  
 function Teleporter_Event(event, player, unit, sender, intid, code)
-        if(intid == 0) then
-                Teleporter_Gossip(event, player, unit)
-        elseif(intid <= 10) then
-                for i, v in ipairs(T[intid]) do
-                        if (i > 2) then
-                                player:GossipMenuAddItem(0, v[1], 0, intid..i)
-                        end
-                end
-                player:GossipMenuAddItem(0, "Back", 0, 0)
-                player:GossipSendMenu(1, unit)
-        elseif(intid > 10) then
-                for i = 1, #T do
-                        for j, v in ipairs(T[i]) do
-                                if(intid == tonumber(i..j)) then
-                                        player:GossipComplete()
-                                        player:Teleport(v[2], v[3], v[4], v[5], v[6])
-                                end
-                        end
-                end
+    if (intid == 0) then
+        Teleporter_Gossip(event, player, unit)
+    elseif(intid <= 10) then
+        for i, v in ipairs(T[intid]) do
+            if (i > 2) then
+                player:GossipMenuAddItem(0, v[1], 0, intid..i)
+            end
         end
+        player:GossipMenuAddItem(0, "Back", 0, 0)
+        player:GossipSendMenu(1, unit)
+    elseif(intid > 10) then
+        for i = 1, #T do
+            for j, v in ipairs(T[i]) do
+                if (intid == tonumber(i..j)) then
+                    player:GossipComplete()
+                    player:Teleport(v[2], v[3], v[4], v[5], v[6])
+                end
+            end
+        end
+    end
 end
  
 RegisterCreatureGossipEvent(UnitEntry, 1, Teleporter_Gossip)
