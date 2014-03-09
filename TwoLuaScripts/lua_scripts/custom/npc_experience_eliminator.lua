8159e3eac569a39546bf012c50bb28a84410d94f
Salja <salja8840@gmail.com>
2014-03-09 20:06:32 +0100

    fix tabbs
    Signed-off-by: Salja <salja8840@gmail.com>

--- TwoLuaScripts/lua_scripts/custom/npc_experience_eliminator.lua
+++ TwoLuaScripts/lua_scripts/custom/npc_experience_eliminator.lua
@@ -9,7 +9,7 @@ local function GossipHello_ExperienceEliminator(event, player, unit)
     if (player:HasFlag(190, 524288)) then
         player:GossipMenuAddItem(0, "I want to be able to gain experience again.", 0, 1)
     else
-	    player:GossipMenuAddItem(0, "I don't want to gain experience anymore.", 0, 2)
+        player:GossipMenuAddItem(0, "I don't want to gain experience anymore.", 0, 2)
     end
 	
     player:GossipMenuAddItem(0, "I need nothing...", 0, 3)
@@ -19,20 +19,20 @@ end
 
 local function GossipSelect_ExperienceEliminator(event, player, unit, sender, intid, code)
     if (intid == 1) then
-	    if (player:GetCoinage() < MoneyCount) then
-			unit:SendUnitSay("Too little money "..player:GetName().." come back when you have "..MoneyCount.." Chopper!", 0)
-        else
-		    player:ModifyMoney(-MoneyCount)
-		    player:RemoveFlag(190, 524288)
-            unit:SendUnitSay("Thanks! "..player:GetName().." see you.", 0)
-        end
-		player:GossipComplete()
-    elseif (intid == 2) then
-	    if (player:GetCoinage() < MoneyCount) then
+        if (player:GetCoinage() < MoneyCount) then
             unit:SendUnitSay("Too little money "..player:GetName().." come back when you have "..MoneyCount.." Chopper!", 0)
         else
-		    player:ModifyMoney(-MoneyCount)
-		    player:SetFlag(190, 524288)
+            player:ModifyMoney(-MoneyCount)
+            player:RemoveFlag(190, 524288)
+            unit:SendUnitSay("Thanks! "..player:GetName().." see you.", 0)
+        end
+        player:GossipComplete()
+    elseif (intid == 2) then
+        if (player:GetCoinage() < MoneyCount) then
+            unit:SendUnitSay("Too little money "..player:GetName().." come back when you have "..MoneyCount.." Chopper!", 0)
+        else
+            player:ModifyMoney(-MoneyCount)
+            player:SetFlag(190, 524288)
             unit:SendUnitSay("Thanks! "..player:GetName().." see you.", 0)
         end
         player:GossipComplete()
