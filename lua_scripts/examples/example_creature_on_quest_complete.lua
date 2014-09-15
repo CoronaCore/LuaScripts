c149f3334109204e0e1bb8c95bb0568143d7a89a
Salja <salja8840@gmail.com>
2014-09-15 20:03:16 +0200

    Update example_creature_on_quest_complete.lua

--- lua_scripts/examples/example_creature_on_quest_complete.lua
+++ lua_scripts/examples/example_creature_on_quest_complete.lua
@@ -1,12 +1,12 @@
 local NpcId =
 local QuestId =
 
-local SAY_01 = "Example OnQuestComplete Works"
+local SAY_01 = "Example OnQuestReward Works"
 
-local function OnQuestComplete(event, player, creature, quest)
+local function OnQuestReward(event, player, creature, quest)
     if (quest:GetId() == QuestId) then
         creature:SendUnitSay(SAY_01, 0)
     end
 end
 
-RegisterCreatureEvent(NpcId, 33, OnQuestComplete)
+RegisterCreatureEvent(NpcId, 34, OnQuestReward)
