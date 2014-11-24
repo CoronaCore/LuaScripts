33f2c448fdde3239eaf99425bd2084c21ca5ffaf
Salja <salja8840@gmail.com>
2014-11-24 19:19:41 +0100

    Commands
    Signed-off-by: Salja <salja8840@gmail.com>

--- lua_scripts/examples/example_creature_fight_phase.lua
+++ lua_scripts/examples/example_creature_fight_phase.lua
@@ -2,29 +2,29 @@ local NPC_EXAMPLE=
 
 local NpcExample = {}
 
-function NpcExample.OnEnterCombat(event, creature, target)        -- Start Phase 1
+function NpcExample.OnEnterCombat(event, creature, target)        -- Start Phase 1 at 100% Health
     creature:SendUnitSay("Phase 1", 0, target)
-    creature:RegisterEvent(NpcExample.OnPhase_2, 1000, 0)
+    creature:RegisterEvent(NpcExample.OnPhase_2, 1000, 0)         -- Check for next Phase
 end
 
-function NpcExample.OnPhase_2(event, delay, pCall, creature)      -- Start Phase 2
-    if (creature:GetHealthPct() < 75) then
+function NpcExample.OnPhase_2(event, delay, pCall, creature)
+    if (creature:GetHealthPct() < 75) then                        -- Start Phase 2 at 75% Health
         creature:RemoveEvents()
         creature:SendUnitSay("Phase 2", 0, target)
-        creature:RegisterEvent(NpcExample.OnPhase_3, 1000, 0)
+        creature:RegisterEvent(NpcExample.OnPhase_3, 1000, 0)     -- Check for next Phase
     end
 end
 
-function NpcExample.OnPhase_3(event, delay, pCall, creature)      -- Start Phase 3
-    if (creature:GetHealthPct() < 50) then
+function NpcExample.OnPhase_3(event, delay, pCall, creature)
+    if (creature:GetHealthPct() < 50) then                        -- Start Phase 3 at 50% Health
         creature:RemoveEvents()
         creature:SendUnitSay("Phase 3", 0, target)
-        creature:RegisterEvent(NpcExample.OnPhase_4, 1000, 0)
+        creature:RegisterEvent(NpcExample.OnPhase_4, 1000, 0)     -- Check for next Phase
     end
 end
 
-function NpcExample.OnPhase_4(event, delay, pCall, creature)      -- Start Phase 4
-    if (creature:GetHealthPct() < 25) then
+function NpcExample.OnPhase_4(event, delay, pCall, creature)
+    if (creature:GetHealthPct() < 25) then                        -- Start Phase 4 at 25% Health
         creature:RemoveEvents()
         creature:SendUnitSay("Phase 4", 0, target)
     end
