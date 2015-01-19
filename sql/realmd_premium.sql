9c863ea9c82f466318f340d66ad41f7f13ee2f61
Salja <salja8840@gmail.com>
2015-01-19 11:34:17 +0100

    fix sql
    Signed-off-by: Salja <salja8840@gmail.com>

--- sql/realmd_premium.sql
+++ sql/realmd_premium.sql
@@ -19,7 +19,7 @@ INSERT INTO `account_premium`  (`id`) SELECT id FROM `account`;
 DROP TABLE IF EXISTS `account_premium_codes`;
 CREATE TABLE `account_premium_codes` (
   `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `code` INT(11) UNSIGNED NOT NULL DEFAULT '0',
+  `code` CHAR(100) NOT NULL DEFAULT '0',
   `type` INT(11) UNSIGNED NOT NULL DEFAULT '0',
   `premium_time` INT(11) UNSIGNED NOT NULL DEFAULT '0',
   `premium_rank` INT(11) UNSIGNED NOT NULL DEFAULT '0',
@@ -31,3 +31,15 @@ CREATE TABLE `account_premium_codes` (
   `active` INT(11) UNSIGNED NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=MYISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- Example for Premium Time + 30 Days
+INSERT INTO `account_premium_codes` VALUES('1','WBt8fKpW','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
+INSERT INTO `account_premium_codes` VALUES('2','U7MacUUY','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
+INSERT INTO `account_premium_codes` VALUES('3','Dcq7G7A6','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
+INSERT INTO `account_premium_codes` VALUES('4','ya2phXzj','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
+INSERT INTO `account_premium_codes` VALUES('5','xCpEUzY7','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
+INSERT INTO `account_premium_codes` VALUES('6','hNx4rvWX','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
+INSERT INTO `account_premium_codes` VALUES('7','A2RL9W4W','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
+INSERT INTO `account_premium_codes` VALUES('8','9NnFpzao','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
+INSERT INTO `account_premium_codes` VALUES('9','ZHUjvxqA','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
+INSERT INTO `account_premium_codes` VALUES('10','CTPDot64','0','30','0','0','0','0','0','0000-00-00 00:00:00','1');
