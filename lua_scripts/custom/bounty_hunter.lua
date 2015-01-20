56a5a3d0ebac14fe04b41650310ba0d07cc6427a
Salja <salja8840@gmail.com>
2015-01-21 00:17:00 +0100

    few rewirtes
    Signed-off-by: Salja <salja8840@gmail.com>

--- lua_scripts/custom/bounty_hunter.lua
+++ lua_scripts/custom/bounty_hunter.lua
@@ -4,66 +4,75 @@
 -- Include sc_default
 require "base/sc_default"
 
-BountyHunterSystem = {}
+local BountyHunter = {}
 
-BountyHunterSystemName = "|CFF18E7BD[Bounty Hunter System]|r"
-
-BountyHunterSystem.Settings = {
+BountyHunter.Settings = {
+    Name = "|CFF18E7BD[BountyHunter]|r",
     NpcEntry = 60000,
     BountyMoney = 100000,
+    HunterMinLevel = 70,
+    BountyMinLevel = 70,
     BountyCommand = "#bounty list",
 };
 
-function BountyHunterSystem.OnGossipHello(event, player, unit)
-    player:GossipMenuAddItem(0, "Set a Bounty for "..BountyHunterSystem.Settings.BountyMoney.." Chopper", 1, 1, true, nil)
-    player:GossipMenuAddItem(0, "Bounty List", 0, 2)
-    player:GossipSetText("Greetings "..player:GetName().."")
+function BountyHunter.OnGossipHello(event, player, unit)
+    local PlayerLevel = player:GetLevel()
+    local PlayerName = player:GetName()
+
+    if PlayerLevel < BountyHunter.Settings.HunterMinLevel then
+        player:GossipSetText(string.format("Greetings %s you need level %s", PlayerName, BountyHunter.Settings.HunterMinLevel))
+    else
+        player:GossipMenuAddItem(0, "Set a Bounty for "..BountyHunter.Settings.BountyMoney.." Chopper", 1, 1, true, nil)
+        player:GossipMenuAddItem(0, "Bounty List", 0, 2)
+        player:GossipSetText(string.format("Greetings %s", PlayerName))
+    end
     player:GossipSendMenu(0x7FFFFFFF, unit)
 end
 
-function BountyHunterSystem.OnGossipSelect(event, player, unit, sender, intid, code)
+function BountyHunter.OnGossipSelect(event, player, unit, sender, intid, code)
     local result = CharDBQuery("SELECT * FROM character_bounty ORDER BY id ASC")
 
     if (intid == 1) then
-        local BountyCheck = CharDBQuery(string.format("SELECT * FROM character_bounty WHERE bounty_name = ('%s')", code))
+        local result1 = CharDBQuery(string.format("SELECT * FROM character_bounty WHERE bounty_name = ('%s')", code))
         local Bounty = GetPlayerByName(code)
         local BountyName = code
         local HunterName = player:GetName()
 
-        if Bounty then
+        if Bounty and Bounty:GetLevel() >= BountyHunter.Settings.BountyMinLevel then
+            print(Bounty:GetLevel())
             if BountyName == HunterName then
-                player:SendBroadcastMessage(string.format("%s You cant set a bounty on your self", BountyHunterSystemName))
+                player:SendBroadcastMessage(string.format("%s You cant set a bounty on your self", BountyHunter.Settings.Name))
                 player:GossipComplete()
             else
-                if result ~= nil and BountyCheck ~= nil then
-                    if BountyName == BountyCheck:GetString(1) then
-                        player:SendBroadcastMessage(string.format("%s On |CFFFF0000%s|r is already a bounty", BountyHunterSystemName, BountyName))
+                if result ~= nil and result1 ~= nil then
+                    if BountyName == result1:GetString(1) then
+                        player:SendBroadcastMessage(string.format("%s On |CFFFF0000%s|r is already a bounty", BountyHunter.Settings.Name, BountyName))
                         player:GossipComplete()
                     else
-                        if player:GetCoinage() >= BountyHunterSystem.Settings.BountyMoney then
-                            player:SendBroadcastMessage(string.format("%s Set a bounty on |CFFFF0000%s|r", BountyHunterSystemName, BountyName))
+                        if player:GetCoinage() >= BountyHunter.Settings.BountyMoney then
+                            player:SendBroadcastMessage(string.format("%s Set a bounty on |CFFFF0000%s|r", BountyHunter.Settings.Name, BountyName))
                             CharDBQuery(string.format("INSERT INTO `character_bounty` (`bounty_name`, `hunter_name`) VALUES('%s','%s')", BountyName, HunterName))
-                            Bounty:SendBroadcastMessage(string.format("%s |CFFFF0000%s|r set a bounty on you", BountyHunterSystemName, HunterName))
-                            player:ModifyMoney(-BountyHunterSystem.Settings.BountyMoney)
+                            Bounty:SendBroadcastMessage(string.format("%s |CFFFF0000%s|r set a bounty on you", BountyHunter.Settings.Name, HunterName))
+                            player:ModifyMoney(-BountyHunter.Settings.BountyMoney)
                             player:GossipComplete()
                         else
-                            player:SendBroadcastMessage(string.format("%s You do not have enough money", BountyHunterSystemName))
+                            player:SendBroadcastMessage(string.format("%s You do not have enough money", BountyHunter.Settings.Name))
                         end
                     end
                 else
-                    if player:GetCoinage() >= BountyHunterSystem.Settings.BountyMoney then
-                        player:SendBroadcastMessage(string.format("%s Set a bounty on |CFFFF0000%s|r", BountyHunterSystemName, BountyName))
+                    if player:GetCoinage() >= BountyHunter.Settings.BountyMoney then
+                        player:SendBroadcastMessage(string.format("%s Set a bounty on |CFFFF0000%s|r", BountyHunter.Settings.Name, BountyName))
                         CharDBQuery(string.format("INSERT INTO `character_bounty` (`bounty_name`, `hunter_name`) VALUES('%s','%s')", BountyName, HunterName))
-                        Bounty:SendBroadcastMessage(string.format("%s |CFFFF0000%s|r set a bounty on you", BountyHunterSystemName, HunterName))
-                        player:ModifyMoney(-BountyHunterSystem.Settings.BountyMoney)
+                        Bounty:SendBroadcastMessage(string.format("%s |CFFFF0000%s|r set a bounty on you", BountyHunter.Settings.Name, HunterName))
+                        player:ModifyMoney(-BountyHunter.Settings.BountyMoney)
                         player:GossipComplete()
                     else
-                        player:SendBroadcastMessage(string.format("%s You do not have enough money", BountyHunterSystemName))
+                        player:SendBroadcastMessage(string.format("%s You do not have enough money", BountyHunter.Settings.Name))
                     end 
                 end
             end
         else
-            player:SendBroadcastMessage(string.format("%s Player with name |CFFFF0000%s|r not found in world", BountyHunterSystemName, BountyName))
+            player:SendBroadcastMessage(string.format("%s Player with name |CFFFF0000%s|r not found or player have not level |CFFFF0000%s|r.", BountyHunter.Settings.Name, BountyName, BountyHunter.Settings.BountyMinLevel))
             player:GossipComplete()
         end
     end
@@ -71,45 +80,45 @@ function BountyHunterSystem.OnGossipSelect(event, player, unit, sender, intid, c
     if (intid == 2) then
         if result ~= nil then
             repeat
-                player:SendBroadcastMessage(string.format("%s Bounty: = |CFFFF0000%s|r  / Hunter: = |CFFFF0000%s|r", BountyHunterSystemName, result:GetString(1), result:GetString(2)))
+                player:SendBroadcastMessage(string.format("%s Bounty: = |CFFFF0000%s|r  / Hunter: = |CFFFF0000%s|r", BountyHunter.Settings.Name, result:GetString(1), result:GetString(2)))
             until not result:NextRow()
         else
-            player:SendBroadcastMessage(string.format("%s No bounty found", BountyHunterSystemName))
+            player:SendBroadcastMessage(string.format("%s No bounty found", BountyHunter.Settings.Name))
         end
     end
     player:GossipComplete()
 end
 
-function BountyHunterSystem.OnChatCommand(event, player, msg, _, lang)
+function BountyHunter.OnChatCommand(event, player, msg, _, lang)
     local result = CharDBQuery("SELECT * FROM character_bounty ORDER BY id ASC")
 
-    if (msg == BountyHunterSystem.Settings.BountyCommand) then
+    if (msg == BountyHunter.Settings.BountyCommand) then
         if result ~= nil then
             repeat
-                player:SendBroadcastMessage(string.format("%s Bounty: = |CFFFF0000%s|r  / Hunter: = |CFFFF0000%s|r", BountyHunterSystemName, result:GetString(1), result:GetString(2)))
+                player:SendBroadcastMessage(string.format("%s Bounty: = |CFFFF0000%s|r  / Hunter: = |CFFFF0000%s|r", BountyHunter.Settings.Name, result:GetString(1), result:GetString(2)))
             until not result:NextRow()
         else
-            player:SendBroadcastMessage(string.format("%s No bounty found", BountyHunterSystemName))
+            player:SendBroadcastMessage(string.format("%s No bounty found", BountyHunter.Settings.Name))
         end
         return false
    end
 end
 
-function BountyHunterSystem.OnPlayerKill(event, killer, killed)
-    local BountyNameCheck = CharDBQuery(string.format("SELECT * FROM character_bounty WHERE bounty_name = ('%s')", killed:GetName()))
+function BountyHunter.OnPlayerKill(event, killer, killed)
+    local result = CharDBQuery(string.format("SELECT * FROM character_bounty WHERE bounty_name = ('%s')", killed:GetName()))
     local BountyName = killed:GetName()
  
-    if BountyNameCheck ~= nil then
-        if BountyName == BountyNameCheck:GetString(1) then
-            killed:SendBroadcastMessage(string.format("%s You remove from bounty list", BountyHunterSystemName))
-            killer:SendBroadcastMessage(string.format("%s On |CFFFF0000%s|r was a bounty you become |CFFFF0000%s|r Chopper", BountyHunterSystemName, BountyName, BountyHunterSystem.Settings.BountyMoney))
+    if result ~= nil then
+        if BountyName == result:GetString(1) then
+            killed:SendBroadcastMessage(string.format("%s You remove from bounty list", BountyHunter.Settings.Name))
+            killer:SendBroadcastMessage(string.format("%s On |CFFFF0000%s|r was a bounty you become |CFFFF0000%s|r Chopper", BountyHunter.Settings.Name, BountyName, BountyHunter.Settings.BountyMoney))
             CharDBQuery(string.format("DELETE FROM `character_bounty` WHERE bounty_name = '%s'", BountyName))
-            killer:ModifyMoney(BountyHunterSystem.Settings.BountyMoney)
+            killer:ModifyMoney(BountyHunter.Settings.BountyMoney)
         end
     end
 end
 
-RegisterPlayerEvent(6, BountyHunterSystem.OnPlayerKill)
-RegisterPlayerEvent(18, BountyHunterSystem.OnChatCommand)
-RegisterCreatureGossipEvent(BountyHunterSystem.Settings.NpcEntry, 1, BountyHunterSystem.OnGossipHello)
-RegisterCreatureGossipEvent(BountyHunterSystem.Settings.NpcEntry, 2, BountyHunterSystem.OnGossipSelect)
+RegisterPlayerEvent(6, BountyHunter.OnPlayerKill)
+RegisterPlayerEvent(18, BountyHunter.OnChatCommand)
+RegisterCreatureGossipEvent(BountyHunter.Settings.NpcEntry, 1, BountyHunter.OnGossipHello)
+RegisterCreatureGossipEvent(BountyHunter.Settings.NpcEntry, 2, BountyHunter.OnGossipSelect)
--- lua_scripts/custom/pvp_token.lua
+++ lua_scripts/custom/pvp_token.lua
@@ -5,59 +5,60 @@
 -- Include sc_default
 require "base/sc_default"
 
-PvPToken = {}
+local TokenSystem = {}
 
 -- Settings
-PvPToken.Settings = {
+TokenSystem.Settings = {
+    Name = "[Token System]",
     ItemOrGold = 0,                     -- 0 = Gold and Item, 1 = Only Item, 2 = Only Gold
     WorldAnnounce = true,               -- Sends a World Announce false = Off, true = On
     GoldCount = 10000,                  -- Reward in copper
     ItemEntry = 20558,                  -- Warsong Gulch Mark of Honor
     ItemCount = 1,                      -- Count of the Item
     ItemName = GetItemLink(20558),
-    KillCooldown = 180,                 -- Wehn you kill the same player in this time you become not gold or item (set to 0 for disable)
+    Cooldown = 180,                     -- Wehn you kill the same player in this time you become not gold or item (set to 0 for disable)
 };
 
-function PvPToken.OnPlayerKill(event, killer, killed)
+function TokenSystem.OnPlayerKill(event, killer, killed)
     local Receiver = killer:GetGUIDLow()
     local KillerName = killer:GetName()
     local KilledName = killed:GetName()
 
-    if (PvPToken.Settings.WorldAnnounce == true) then
-        SendWorldMessage(string.format("[PVP] |Hplayer:%s|h[%s]|h killed |Hplayer:%s|h[%s]|h", KillerName, KillerName, KilledName, KilledName))
+    if (TokenSystem.Settings.WorldAnnounce == true) then
+        SendWorldMessage(string.format("%s |Hplayer:%s|h[%s]|h killed |Hplayer:%s|h[%s]|h", TokenSystem.Settings.Name, KillerName, KillerName, KilledName, KilledName))
     end
 
-    if (PvPToken.Settings.ItemOrGold ~= 2) then
+    if (TokenSystem.Settings.ItemOrGold ~= 2) then
         if killed:GetLuaCooldown() == 0 then
-            if (killer:AddItem(PvPToken.Settings.ItemEntry, PvPToken.Settings.ItemCount)) then
-                if (PvPToken.Settings.ItemCount == 1) then
-                    killer:SendBroadcastMessage("|CFF20C000 You get: "..PvPToken.Settings.ItemName.."|CFF20C000.|r")
-                    killed:SetLuaCooldown(PvPToken.Settings.KillCooldown)
+            if (killer:AddItem(TokenSystem.Settings.ItemEntry, TokenSystem.Settings.ItemCount)) then
+                if (TokenSystem.Settings.ItemCount == 1) then
+                    killer:SendBroadcastMessage(string.format("%s |CFF20C000 You get: %s|CFF20C000.|r", TokenSystem.Settings.Name, TokenSystem.Settings.ItemName))
+                    killed:SetLuaCooldown(TokenSystem.Settings.Cooldown)
                 else
-                    killer:SendBroadcastMessage("|CFF20C000 You get: "..PvPToken.Settings.ItemName.."|CFF20C000 x"..PvPToken.Settings.ItemCount..".|r")
-                    killed:SetLuaCooldown(PvPToken.Settings.KillCooldown)
+                    killer:SendBroadcastMessage(string.format("%s |CFF20C000 You get: %s|CFF20C000 x%s.|r", TokenSystem.Settings.Name, TokenSystem.Settings.ItemNamem, TokenSystem.Settings.ItemCount))
+                    killed:SetLuaCooldown(TokenSystem.Settings.Cooldown)
                 end
             else
-                killer:SendBroadcastMessage("|cffff0000 Your bags are full, we will send it by mail.|r")
-                SendMail("PvP Token System", "Your Bags are full we send via Mail", Receiver, nil, 41, nil, PvPToken.Settings.ItemEntry, PvPToken.Settings.ItemCount)
-                killed:SetLuaCooldown(PvPToken.Settings.KillCooldown)
+                killer:SendBroadcastMessage(string.format("%s |cffff0000 Your bags are full, we will send it by mail.|r", TokenSystem.Settings.Name))
+                SendMail("Token System", "Your Bags are full we send via Mail", Receiver, nil, 41, nil, TokenSystem.Settings.ItemEntry, TokenSystem.Settings.ItemCount)
+                killed:SetLuaCooldown(TokenSystem.Settings.Cooldown)
             end
         else
-            killer:SendBroadcastMessage(string.format("Set Cooldown for %s, to %s seconds for next token kill", KilledName, math.ceil(killed:GetLuaCooldown())))
-            killed:SendBroadcastMessage(string.format("Set YOUR Cooldown, to %s seconds for next token kill", math.ceil(killed:GetLuaCooldown())))
+            killer:SendBroadcastMessage(string.format("%s Set Cooldown for %s, to %s seconds for next token kill", TokenSystem.Settings.Name, KilledName, math.ceil(killed:GetLuaCooldown())))
+            killed:SendBroadcastMessage(string.format("%s Set YOUR Cooldown, to %s seconds for next token kill", TokenSystem.Settings.Name, math.ceil(killed:GetLuaCooldown())))
         end
     end
 
-    if (PvPToken.Settings.ItemOrGold ~= 1) then
+    if (TokenSystem.Settings.ItemOrGold ~= 1) then
         if killed:GetLuaCooldown() == 0 then
-            killer:ModifyMoney(PvPToken.Settings.GoldCount)
-            killer:SendBroadcastMessage("|CFF20C000 You get: "..PvPToken.Settings.GoldCount.."|CFF20C000 Copper.|r")
-            killed:SetLuaCooldown(PvPToken.Settings.KillCooldown)
+            killer:ModifyMoney(TokenSystem.Settings.GoldCount)
+            killer:SendBroadcastMessage(string.format("%s |CFF20C000 You get: %s|CFF20C000 Copper.|r", TokenSystem.Settings.Name, TokenSystem.Settings.GoldCount))
+            killed:SetLuaCooldown(TokenSystem.Settings.Cooldown)
         else
-            killer:SendBroadcastMessage(string.format("Set Cooldown for %s, to %s seconds for next token kill", KilledName, math.ceil(killed:GetLuaCooldown())))
-            killed:SendBroadcastMessage(string.format("Set YOUR Cooldown, to %s seconds for next token kill", math.ceil(killed:GetLuaCooldown())))
+            killer:SendBroadcastMessage(string.format("%s Set Cooldown for %s, to %s seconds for next token kill", TokenSystem.Settings.Name, KilledName, math.ceil(killed:GetLuaCooldown())))
+            killed:SendBroadcastMessage(string.format("%s Set YOUR Cooldown, to %s seconds for next token kill", TokenSystem.Settings.Name, math.ceil(killed:GetLuaCooldown())))
         end
     end
 end
 
-RegisterPlayerEvent(6, PvPToken.OnPlayerKill)
+RegisterPlayerEvent(6, TokenSystem.OnPlayerKill)
